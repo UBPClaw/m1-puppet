68aab6dbfca5a539bd2d589ffd7a156bd38667f4
root <root@powc-pupt-01-dv.corp.mitchellrepair.com>
2016-05-16 15:03:41 -0700

    repo

--- mric/manifests/repo.pp
+++ mric/manifests/repo.pp
@@ -1,26 +1,15 @@
 #Configure the M1 custom RHEL repos based on OS major version
 class mric::repo inherits mric {
+  
+  $repo_server = $::operatingsystemmajrelease ? {
+    '5' => 'ftp://powc-repo-01-pv.corp.mitchellrepair.com',
+    '6' => 'ftp://powc-repo-02-pv.corp.mitchellrepair.com',
+    '7' => 'ftp://powc-repo-03-pv.corp.mitchellrepair.com',
+  }
 
-if $operatingsystemmajrelease == '6' {
-    file { 'm1.repo':
-      ensure   => file,
-      path     => '/etc/yum.repos.d/m1.repo',
-      content  => "[Mitchell1]
-name=Mitchell1 RHEL 6.x Repository
-baseurl=ftp://powc-repo-02-pv.corp.mitchellrepair.com/
-gpgcheck=0
-enabled=1",
-    }
-} 
-if $operatingsystemmajrelease == '7' {
-    file {'m1.repo':
-      ensure   => file,
-      path     => '/etc/yum.repos.d/m1.repo',
-      content  => "[Mitchell1]
-name=Mitchell1 RHEL 7.x Repository
-baseurl=ftp://powc-repo-03-pv.corp.mitchellrepair.com/
-gpgcheck=0
-enabled=1",
-      }
-   }
+  file { 'm1.repo':
+    ensure => file,
+    path   => '/etc/yum.repos.d/m1.repo',
+    content => template('mric/m1.repo.erb'),
+  }
 }
